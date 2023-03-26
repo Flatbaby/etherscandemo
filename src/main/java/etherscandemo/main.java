f8ee3c4b3aab176afe749655e420aeddcea551ca
Flatbaby <60462768+Flatbaby@users.noreply.github.com>
2023-03-26 18:31:43 +0200

    Updates
    Updated the main file and added some DRY methods to make the code cleaner as well as adding multiple working tests

--- src/main/java/etherscandemo/main.java
+++ src/main/java/etherscandemo/main.java
@@ -1,6 +1,7 @@
 package etherscandemo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
@@ -16,48 +17,83 @@ import org.openqa.selenium.interactions.Actions;
 public class main {
 	// for further teardown and setup i initialized the driver here
 	WebDriver driver;
+	// Finding elements for methods
+	// Some of these may repeat as they have the same xpath
+	// and will be reused (fix this later)
+	By loginButton = By.xpath("//html[@id='html']//input[@id='ContentPlaceHolder1_btnRegister']");
+	By cookiesButton = By.xpath("//html[@id='html']//button[@id='btnCookie']");
+	By usernameField = By.xpath("//html[@id='html']//input[@id='ContentPlaceHolder1_txtUserName']");
+	By usernameError = By.xpath("//html[@id='html']//div[@id='ContentPlaceHolder1_txtUserName-error']");
+	By emailField = By.xpath("//html[@id='html']//input[@id='ContentPlaceHolder1_txtEmail']");
+	By invalidEmailElement = By.xpath("//html[@id='html']//div[@id='ContentPlaceHolder1_txtEmail-error']");
+	By confirmationEmailField = By.xpath("//html[@id='html']//input[@id='ContentPlaceHolder1_txtConfirmEmail']");
+	By confirmationEmailFieldError = By
+			.xpath("//html[@id='html']//div[@id='ContentPlaceHolder1_txtConfirmEmail-error']");
 
 	@Test
 	public void emptyClick() throws InterruptedException {
-		//webdrivermanager to setup the chrome driver
+		click(cookiesButton);
+		forceButton();
+		WebElement userError = find(usernameError);
+		elementPresentAssertion(true, userError);
+	}
+
+	@Test
+	public void validUsername() {
+		type(usernameField, "username");
+		WebElement userError = find(usernameError);
+		elementPresentAssertion(false, userError);
+	}
+
+	@Test
+	public void shortUsername() {
+		type(usernameField, "12");
+		WebElement userError = find(usernameError);
+		elementPresentAssertion(true, userError);
+	}
+
+	@Test
+	public void nonAlphanumericUsername() {
+		type(usernameField, "+_)()_(");
+		WebElement userError = find(usernameError);
+		elementPresentAssertion(true, userError);
+	}
+
+	@Test
+	public void invalidEmail() {
+		type(emailField, "false");
+		WebElement invalidEmail = find(invalidEmailElement);
+		elementPresentAssertion(true, invalidEmail);
+	}
+
+	@Test
+	public void validEmail() {
+		type(emailField, "true@gmail.com");
+		WebElement invalidEmail = find(invalidEmailElement);
+		elementPresentAssertion(false, invalidEmail);
+	}
+
+	@Test
+	public void invalidEmailWithConfirmation() {
+		type(emailField, "false");
+		type(confirmationEmailField, "false");
+		WebElement invalidEmail = find(invalidEmailElement);
+		WebElement invalidEmailConfirmation = find(confirmationEmailFieldError);
+		elementPresentAssertion(true, invalidEmail);
+		elementPresentAssertion(true, invalidEmailConfirmation);
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		// webdrivermanager to setup the chrome driver
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
 		// i was encountering an error and this resolved it
 		options.addArguments("--remote-allow-origins=*");
 		driver = new ChromeDriver(options);
-		//maximising the webbrowser to exclude any errors due to size and interceptions
+		// maximising the webbrowser to exclude any errors due to size and interceptions
 		driver.manage().window().maximize();
-		//fetching the site
 		driver.get("https://etherscan.io/register");
-		WebElement button = driver.findElement(By.xpath("//html[@id='html']//input[@id='ContentPlaceHolder1_btnRegister']"));
-		WebElement cookies = driver.findElement(By.xpath("//html[@id='html']//button[@id='btnCookie']"));
-		WebElement username = driver.findElement(By.xpath("//html[@id='html']//input[@id='ContentPlaceHolder1_txtUserName']"));
-		cookies.click();
-		Thread.sleep(3000);
-		username.click();
-		username.sendKeys("username");
-		boolean clicked = false;
-		do {
-		try {
-		button.click();
-		}
-		catch(WebDriverException e){
-			continue;
-		}
-		
-		finally {
-			clicked = true;
-			System.out.println("button got fucking clicked");
-			Thread.sleep(2000);
-		}
-		} while (!clicked);
-		WebElement usernameError = driver.findElement(By.xpath("//html[@id='html']//div[@id='ContentPlaceHolder1_txtUserName-error']"));
-		String userError = usernameError.getText();
-		Assert.assertEquals(null, userError);
-	}
-
-	@BeforeMethod
-	public void beforeMethod() {
 		System.out.println("Starting test: ");
 	}
 
@@ -65,6 +101,37 @@ public class main {
 	public void afterMethod() {
 		System.out.println("Closing browser");
 		driver.quit();
+	}
+
+	// Methods as per DRY principlex
+	WebElement find(By locator) {
+		return driver.findElement(locator);
+	}
+
+	private void click(By locator) {
+		find(locator).click();
+	}
+
+	private void type(By locator, String text) {
+		find(locator).sendKeys(text);
+	}
+
+	private void forceButton() {
+		WebElement button = driver.findElement(loginButton);
+		// forcing a button click due to the Cookies message covering up the button.
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click();", button);
+	}
+
+	private void elementPresentAssertion(boolean appearance, WebElement element) {
+		try {
+			Assert.assertEquals(appearance, element.isDisplayed());
+		} catch (AssertionError e) {
+			System.out.println("Error");
+			throw e;
+		}
+		System.out.println("Valid test.");
+
 	}
 
 }
